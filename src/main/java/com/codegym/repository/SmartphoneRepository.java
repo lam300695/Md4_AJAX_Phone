13cd656697b5be9788e9c8e950fbb4c8773ebe1a
lam300695 <lam300695@gmail.com>
2020-04-09 00:59:18 +0700

    Md4_Ajax_JQuerry_smartPhone_manage

---
+++ src/main/java/com/codegym/repository/SmartphoneRepository.java
@@ -0,0 +1,7 @@
+package com.codegym.repository;
+
+import com.codegym.model.Smartphone;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface SmartphoneRepository extends PagingAndSortingRepository<Smartphone,Integer> {
+}
